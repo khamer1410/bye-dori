ada0681e695dab63e33b8a03274175466ef37df2
Krzysztof Hamerszmit <krzysztof.hamerszmit@gmail.com>
2022-04-07 22:29:16 +0200

    Basic content placeholders

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 (function() {
   const noBtn = document.querySelector('#no-btn');
 
-  const x = {a: 1}
-
-  const y = x?.b
-
   noBtn.addEventListener('click', () => {
     alert('CLICKED');
   })
