1516e0055c36cbbbcb57dc42a88793788f6a9ba7
Krzysztof Hamerszmit <krzysztof.hamerszmit@gmail.com>
2022-04-09 21:50:08 +0200

    styling , add observer

--- index.js
+++ index.js
@@ -1,36 +1,66 @@
 (function () {
-  fetch("Dori%20meme%20-%20Sheet1.csv")
-    .then((result) => result.text())
-    .then((csvString) => {
-      const memeTemplateElement = document.querySelector("#template--meme");
-      const memeTemplate = memeTemplateElement.cloneNode(true);
-      memeTemplate.removeAttribute("id");
+  function injectMemes() {
+    return fetch("Dori%20meme%20-%20Sheet1.csv")
+      .then((result) => result.text())
+      .then((csvString) => {
+        const memeTemplateElement = document.querySelector("#template--meme");
+        const memeTemplate = memeTemplateElement.cloneNode(true);
+        memeTemplate.removeAttribute("id");
 
-      const memesMatch = [...csvString.matchAll(/^([^,^@]+),(.+),([^,]*)$/gm)];
-      memesMatch.forEach(([_, name, descriptionStr, imageStr]) => {
-        const newMeme = memeTemplate.cloneNode(true);
-        newMeme.querySelector(".name").innerHTML = name;
+        const memesMatch = [
+          ...csvString.matchAll(/^([^,^@]+),(.+),([^,]*)$/gm),
+        ];
+        memesMatch.forEach(([_, name, descriptionStr, imageStr]) => {
+          const newMeme = memeTemplate.cloneNode(true);
+          newMeme.querySelector(".name").innerHTML = name;
 
-        const description = descriptionStr.startsWith('"')
-          ? descriptionStr.substring(1, descriptionStr.length - 1)
-          : descriptionStr;
-        newMeme.querySelector(".description").innerHTML = description;
+          const description = descriptionStr.startsWith('"')
+            ? descriptionStr.substring(1, descriptionStr.length - 1)
+            : descriptionStr;
+          newMeme.querySelector(".description").innerHTML = description;
 
-        let image = imageStr.replace("\r", "");
-        if (image.includes("//giphy.com/")) {
-          const gifId = image.match(/([^-]*)$/g)[0];
-          gifId && (image = `https://i.giphy.com/media/${gifId}/giphy.gif`);
-        }
-        if (image) newMeme.querySelector(".image").src = image;
-        else newMeme.querySelector(".image").remove();
+          let image = imageStr.replace("\r", "");
+          if (image.includes("//giphy.com/")) {
+            const gifId = image.match(/([^-]*)$/g)[0];
+            gifId && (image = `https://i.giphy.com/media/${gifId}/giphy.gif`);
+          }
+          if (image) newMeme.querySelector(".image").src = image;
+          else newMeme.querySelector(".image").remove();
 
-        memeTemplateElement.parentElement.append(newMeme);
+          memeTemplateElement.parentElement.append(newMeme);
+        });
       });
-    });
+  }
+
+  injectMemes().then(() => {
+    // add intersectionObserver observe memes
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+          } else {
+            entry.target.classList.remove("visible");
+          }
+        });
+      },
+      {
+        rootMargin: "0px 0px -100px 0px",
+        threshold: 0.1,
+      }
+    );
+
+    const memes = document.querySelectorAll(".meme");
+    memes.forEach((meme) => observer.observe(meme));
+  });
 
   const noBtn = document.querySelector("#no-btn");
 
-  noBtn.addEventListener("click", () => {
-    alert("CLICKED");
-  });
+  noBtn.addEventListener("click", alertsMessage);
+
+  function alertsMessage() {
+    alert("Hello");
+  }
+
+
 })();
